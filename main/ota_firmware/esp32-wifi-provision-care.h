c8ae93bde2847cf6a8d0c3708bb4ae0ddc61bb22
mitko125 <154724297+Mitko125@users.noreply.github.com>
2025-03-14 14:00:14 +0200

    Изчистваме 'ota_firmware' от ненужни програми.

--- main/ota_firmware/esp32-wifi-provision-care.h
+++ main/ota_firmware/esp32-wifi-provision-care.h
@@ -7,18 +7,6 @@ extern "C" {
 #include <esp_http_server.h>
 
 /**
- * @brief  Tries to connect to Wi-Fi Access Point with 
- *         credentials stored in default NVS partition
- *         on success just return
- *         on error spawns SoftAP with captive portal
- *
- * @param  ap_ssid_name[32] Wi-Fi AP SSID name NULL terminated.
- *         If empty or NULL AP name set to "esp32-wifi-provision-care-XXXX" XXXX - esp32 MAC addr last digits.
- *
- */
-void wifi_provision_care(char *ap_ssid_name);
-
-/**
  * @brief Register POST handler with desired URI to handle upload firmware over te air
  *        const httpd_uri_t updateota_uri =  { .uri = "/updateota", .method = HTTP_POST, .handler = updateota_post_handler };
  *        httpd_register_uri_handler(server, &updateota_uri);
