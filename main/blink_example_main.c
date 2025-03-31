ae28e229849ba0f106aeb0425b7d43c0270a75ad
mitko125 <154724297+Mitko125@users.noreply.github.com>
2025-03-31 10:20:14 +0300

    Преминаване от spiffs към littlefs.

--- main/blink_example_main.c
+++ main/blink_example_main.c
@@ -15,7 +15,7 @@
 #include "sdkconfig.h"
 #include "nvs_flash.h"
 #include "esp_event.h"
-#include "esp_spiffs.h"
+#include "esp_littlefs.h"
 #include "mdns.h"
 #include "lwip/apps/netbiosns.h"
 
@@ -23,7 +23,7 @@
 
 static const char *TAG = "main";
 
-#define WEB_MOUNT_POINT "/spiffs"
+#define WEB_MOUNT_POINT "/littlefs"
 #define MDNS_HOST_NAME "RGB-Leds"
 #define MDNS_INSTANCE "ELL test web server"
 
@@ -61,29 +61,28 @@ static void configure_led(void)
 
 esp_err_t init_fs(void)
 {
-    esp_vfs_spiffs_conf_t conf = {
+    esp_vfs_littlefs_conf_t conf = {
         .base_path = WEB_MOUNT_POINT,
         .partition_label = NULL,
-        .max_files = 5,
         .format_if_mount_failed = false
     };
-    esp_err_t ret = esp_vfs_spiffs_register(&conf);
+    esp_err_t ret = esp_vfs_littlefs_register(&conf);
 
     if (ret != ESP_OK) {
         if (ret == ESP_FAIL) {
             ESP_LOGE(TAG, "Failed to mount or format filesystem");
         } else if (ret == ESP_ERR_NOT_FOUND) {
-            ESP_LOGE(TAG, "Failed to find SPIFFS partition");
+            ESP_LOGE(TAG, "Failed to find LITTLEFS partition");
         } else {
-            ESP_LOGE(TAG, "Failed to initialize SPIFFS (%s)", esp_err_to_name(ret));
+            ESP_LOGE(TAG, "Failed to initialize LITTLEFS (%s)", esp_err_to_name(ret));
         }
         return ESP_FAIL;
     }
 
     size_t total = 0, used = 0;
-    ret = esp_spiffs_info(NULL, &total, &used);
+    ret = esp_littlefs_info(NULL, &total, &used);
     if (ret != ESP_OK) {
-        ESP_LOGE(TAG, "Failed to get SPIFFS partition information (%s)", esp_err_to_name(ret));
+        ESP_LOGE(TAG, "Failed to get LITTLEFS partition information (%s)", esp_err_to_name(ret));
     } else {
         ESP_LOGI(TAG, "Partition size: total: %d, used: %d", total, used);
     }
